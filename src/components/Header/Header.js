8563dce2bae13a829b4a683b3186d4f709401c16
gracedward <edward.chung.designer@gmail.com>
2023-07-19 11:38:47 +0100

    feat: add context to pass state around

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,30 @@
-'use client';
-import React from 'react';
-import { Volume2, VolumeX } from 'react-feather';
+"use client";
+import React from "react";
+import { Volume2, VolumeX } from "react-feather";
+import { SoundEnabledContext } from "../../components/SoundEnabledProvider";
 
-import VisuallyHidden from '../VisuallyHidden';
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import styles from './Header.module.css';
+import VisuallyHidden from "../VisuallyHidden";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import styles from "./Header.module.css";
 
 function Header() {
+  const { soundEnabled, setSoundEnabled } =
+    React.useContext(SoundEnabledContext);
   const id = React.useId();
-
-  // TODO: Global state?
-  const soundEnabled = true;
 
   return (
     <header className={styles.wrapper}>
-      <MaxWidthWrapper
-        className={styles.innerWrapper}
-      >
+      <MaxWidthWrapper className={styles.innerWrapper}>
         <a href="/">Kool Website</a>
 
         <button
           onClick={() => {
-            // TODO: flip `soundEnabled`
+            setSoundEnabled(!soundEnabled);
           }}
         >
-          {soundEnabled ? (
-            <Volume2 />
-          ) : (
-            <VolumeX />
-          )}
+          {soundEnabled ? <Volume2 /> : <VolumeX />}
           <VisuallyHidden>
-            {soundEnabled
-              ? 'Disable sound effects'
-              : 'Enable sound effects'}
+            {soundEnabled ? "Disable sound effects" : "Enable sound effects"}
           </VisuallyHidden>
         </button>
       </MaxWidthWrapper>
